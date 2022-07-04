9a2b233a156e2c72ecc27d612ae36144b12df1af
Jayesh Sharma <wjayesh@outlook.com>
2022-07-04 19:22:56 +0530

    add cidr variable in rule config

--- eks-s3-seldon-mlflow/vpc.tf
+++ eks-s3-seldon-mlflow/vpc.tf
@@ -23,12 +23,14 @@ module "vpc" {
       from_port        = 3306
       to_port          = 3306
       protocol         = "tcp"
+      cidr_blocks      = "0.0.0.0/0"
     },
     {
       description      = "All Traffic"
       from_port        = 0
       to_port          = 0
       protocol         = -1
+      cidr_blocks      = "0.0.0.0/0"
     }
   ]
   default_security_group_egress = [
