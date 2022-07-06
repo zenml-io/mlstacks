73047717a60cfb81893aa7d48beacd26513e6a8e
Jayesh Sharma <wjayesh@outlook.com>
2022-07-06 18:37:14 +0530

    add seldon

--- gke-gcs-seldon-mlflow/seldon.tf
+++ gke-gcs-seldon-mlflow/seldon.tf
@@ -1,23 +1,23 @@
-# # using the seldon module for creating a 
-# # seldon + istio deployment
-# module "seldon" {
-#     source = "./seldon"
+# using the seldon module for creating a 
+# seldon + istio deployment
+module "seldon" {
+    source = "./seldon"
 
-#     # run only after the eks cluster is set up
-#     depends_on = [module.eks]
+    # run only after the eks cluster is set up
+    depends_on = [module.gke]
 
-#     # details about the seldon deployment
-#     seldon_name = local.seldon.name
-#     seldon_namespace = local.seldon.namespace
+    # details about the seldon deployment
+    seldon_name = local.seldon.name
+    seldon_namespace = local.seldon.namespace
 
-#     # details about the cluster
-#     cluster_endpoint = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     cluster_token = data.aws_eks_cluster_auth.cluster.token
-# }
+    # details about the cluster
+    cluster_endpoint = "https://${module.gke.endpoint}"
+    cluster_ca_certificate = data.google_client_config.default.access_token
+    cluster_token = base64decode(module.gke.ca_certificate)
+}
 
-# resource "kubernetes_namespace" "seldon-workloads" {
-#   metadata {
-#     name = "zenml-seldon-workloads"
-#   }
-# }
+resource "kubernetes_namespace" "seldon-workloads" {
+  metadata {
+    name = "zenml-seldon-workloads"
+  }
+}
