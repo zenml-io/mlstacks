e572499491c4d6f6aa417fb027ad1d58402f374e
Jayesh Sharma <wjayesh@outlook.com>
2022-07-12 12:59:52 +0530

    do a project level iam binding

--- vertex-ai-example/service-account.tf
+++ vertex-ai-example/service-account.tf
@@ -4,45 +4,45 @@ resource "google_service_account" "sa" {
   display_name = "${local.prefix}-${local.service_account.account_id}"
 }
 
-resource "google_service_account_iam_binding" "ai-customcode" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/aiplatform.customCodeServiceAgent"
+resource "google_project_iam_binding" "ai-customcode" {
+  project   = local.project_id
+  role      = "roles/aiplatform.customCodeServiceAgent"
 
   members = [
     "serviceAccount:${google_service_account.sa.email}",
   ]
 }
 
-resource "google_service_account_iam_binding" "ai-serviceagent" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/aiplatform.serviceAgent"
+resource "google_project_iam_binding" "ai-serviceagent" {
+  project   = local.project_id
+  role      = "roles/aiplatform.serviceAgent"
 
   members = [
     "serviceAccount:${google_service_account.sa.email}",
   ]
 }
 
-resource "google_service_account_iam_binding" "container-registry" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/containerregistry.ServiceAgent"
+resource "google_project_iam_binding" "container-registry" {
+  project   = local.project_id
+  role      = "roles/containerregistry.ServiceAgent"
 
   members = [
     "serviceAccount:${google_service_account.sa.email}",
   ]
 }
 
-resource "google_service_account_iam_binding" "secret-manager" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/secretmanager.admin"
+resource "google_project_iam_binding" "secret-manager" {
+  project   = local.project_id
+  role      = "roles/secretmanager.admin"
 
   members = [
     "serviceAccount:${google_service_account.sa.email}",
   ]
 }
 
-resource "google_service_account_iam_binding" "serviceaccount-user" {
-  service_account_id = google_service_account.sa.name
-  role               = "roles/iam.serviceAccountUser"
+resource "google_project_iam_binding" "serviceaccount-user" {
+  project   = local.project_id
+  role      = "roles/iam.serviceAccountUser"
 
   members = [
     "serviceAccount:${google_service_account.sa.email}",
