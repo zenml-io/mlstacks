8a56ce882f21618551b7e9c01068a8ab918a5387
Jayesh Sharma <wjayesh@outlook.com>
2022-07-13 20:22:09 +0530

    refactor outputs and include new values

--- eks-s3-seldon-mlflow/outputs.tf
+++ eks-s3-seldon-mlflow/outputs.tf
@@ -1,33 +1,12 @@
-output "seldon-core-workload-namespace" {
-  value = kubernetes_namespace.seldon-workloads.metadata[0].name
-  description = "The namespace created for hosting your Seldon workloads"
-}
-
-output "seldon-prediction-spec" {
-  value = module.seldon.ingress-gateway-spec
-  sensitive = true
-}
-
-output "s3-bucket-path" {
-  value = aws_s3_bucket.zenml-artifact-store.bucket
-  description = "The S3 bucket path for storing your artifacts"
-}
-
-output "ecr-registry-name" {
-  value = aws_ecr_repository.zenml-ecr-repository[0].name
-  description = "The ECR registry name for storing your images"
-}
-
+# output for eks cluster
 output "eks-cluster-name" {
   value = data.aws_eks_cluster.cluster.name
 }
 
-# outputs for the Mlflow tracking server
-output "ingress-controller-name" {
-  value = module.mlflow.ingress-controller-name
-}
-output "ingress-controller-namespace" {
-  value = module.mlflow.ingress-controller-namespace
+# output for s3 bucket
+output "s3-bucket-path" {
+  value = "s3://${aws_s3_bucket.zenml-artifact-store.bucket}"
+  description = "The S3 bucket path for storing your artifacts"
 }
 
 # outputs for the metadata store
@@ -42,4 +21,36 @@ output "metadata-db-password" {
   value = module.metadata_store.db_instance_password
   sensitive = true
 }
- 
+
+# output for container registry
+output "container-registry-URI" {
+  value = "${data.aws_iam_account_alias.current.id}.dkr.ecr.${local.region}.amazonaws.com"
+}
+output "ecr-registry-name" {
+  value = aws_ecr_repository.zenml-ecr-repository[0].name
+  description = "The ECR registry repository for storing your images"
+}
+
+# outputs for the Mlflow tracking server
+output "ingress-controller-name" {
+  value = module.mlflow.ingress-controller-name
+}
+output "ingress-controller-namespace" {
+  value = module.mlflow.ingress-controller-namespace
+}
+output "mlflow-tracking-URL" {
+  value = data.kubernetes_service.mlflow_tracking.status.0.load_balancer.0.ingress.0.ip
+}
+
+# output for seldon model deployer
+output "seldon-core-workload-namespace" {
+  value = kubernetes_namespace.seldon-workloads.metadata[0].name
+  description = "The namespace created for hosting your Seldon workloads"
+}
+output "seldon-prediction-spec" {
+  value = module.seldon.ingress-gateway-spec
+  sensitive = true
+}
+output "seldon-base-url" {
+  value = data.kubernetes_service.seldon_ingress.status.0.load_balancer.0.ingress.0.ip
+}
