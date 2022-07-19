1a456a40b99fef785e05379a468501d628cc308b
Jayesh Sharma <wjayesh@outlook.com>
2022-07-19 18:49:17 +0530

    don't exit on failure

--- aws-minimal/destroy_recipe.sh
+++ aws-minimal/destroy_recipe.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
-
 main() {
-	INFO='\033[0;34m'
-	NOFORMAT='\033[0m'
+  INFO='\033[0;34m'
+  NOFORMAT='\033[0m'
   ERROR='\033[0;31m'
   WARNING='\033[0;33m'
 
-	# run destroy to clear all resources except the potentially 
+  # run destroy to clear all resources except the potentially 
   # troubling kubernetes resources
-  terraform destroy
+  terraform destroy -auto-approve
 
   # clear dangling k8s resources (temporary hack but simple)
   kubectl delete node --all 
 
   # run terraform destroy again to clean up EKS
-  terraform destroy
+  terraform destroy -auto-approve
 }
 
 msg() {
