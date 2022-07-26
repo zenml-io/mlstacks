5548f014fa0eee708b537f7324e97dfa8d82f587
Jayesh Sharma <wjayesh@outlook.com>
2022-07-26 19:36:52 +0530

    enable relevant APIs

--- gcp-minimal/enable_services.tf
+++ gcp-minimal/enable_services.tf
@@ -1,16 +1,32 @@
-# You must have owner, editor, service config editor roles 
+# You must have owner, editor, or service config editor roles 
 # to be able to enable services.
 
 # enable secret manager
-resource "null_resource" "enable-secretmanager" {
-  provisioner "local-exec" {
-    command = "gcloud services enable secretmanager.googleapis.com --project=${local.project_id}"
-  }
+resource "google_project_service" "secret_manager" {
+  project = local.project_id
+  service = "secretmanager.googleapis.com"
 }
 
 # enable container registry
-resource "null_resource" "enable-containerregistry" {
-  provisioner "local-exec" {
-    command = "gcloud services enable containerregistry.googleapis.com --project=${local.project_id}"
-  }
+resource "google_project_service" "container_registry" {
+  project = local.project_id
+  service = "containerregistry.googleapis.com"
+}
+
+# enable container API
+resource "google_project_service" "container_api" {
+  project = local.project_id
+  service = "container.googleapis.com"
+}
+
+# enable compute engine API
+resource "google_project_service" "compute_engine_api" {
+  project = local.project_id
+  service = "compute.googleapis.com"
+}
+
+# enable cloud resource manager API
+resource "google_project_service" "cloud_resource_manager_api" {
+  project = local.project_id
+  service = "cloudresourcemanager.googleapis.com"
 }
