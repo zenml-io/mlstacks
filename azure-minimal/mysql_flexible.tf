b9758376df0c1112f0817ff4420a8715e0c4078c
Jayesh Sharma <wjayesh@outlook.com>
2022-08-02 01:50:55 +0530

    add private dns to mysql

--- azure-minimal/mysql_flexible.tf
+++ azure-minimal/mysql_flexible.tf
@@ -3,10 +3,11 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
   administrator_login    = var.metadata-db-username
-  administrator_password = var.metadata-db-password == "" ? random_password.mysql_password : var.metadata-db-password
+  administrator_password = var.metadata-db-password == "" ? random_password.mysql_password.result : var.metadata-db-password
   backup_retention_days  = 7
   delegated_subnet_id    = azurerm_subnet.mysql_vnet.id
-
+  private_dns_zone_id    = azurerm_private_dns_zone.mysql_dns.id
+  sku_name               = "B_Standard_B1s"
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "allow_IPs" {
@@ -20,4 +21,16 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "allow_IPs" {
 resource "random_password" "mysql_password" {
   length  = 12
   special = false
+}
+
+resource "azurerm_private_dns_zone" "mysql_dns" {
+  name                = "zenml.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name                  = "mysqllink"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
+  virtual_network_id    = module.network.vnet_id
 }
