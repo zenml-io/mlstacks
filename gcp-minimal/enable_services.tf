d050853b657ce8bb5d25aacf02937846aa33df86
Jayesh Sharma <wjayesh@outlook.com>
2022-08-09 17:38:19 +0530

    disable dependent GCP services on destroy

--- gcp-minimal/enable_services.tf
+++ gcp-minimal/enable_services.tf
@@ -5,7 +5,7 @@
 resource "google_project_service" "secret_manager" {
   project = local.project_id
   service = "secretmanager.googleapis.com"
-  
+
   disable_dependent_services = true
 }
 
@@ -13,7 +13,7 @@ resource "google_project_service" "secret_manager" {
 resource "google_project_service" "container_registry" {
   project = local.project_id
   service = "containerregistry.googleapis.com"
-  
+
   disable_dependent_services = true
 }
 
@@ -21,7 +21,7 @@ resource "google_project_service" "container_registry" {
 resource "google_project_service" "container_api" {
   project = local.project_id
   service = "container.googleapis.com"
-  
+
   disable_dependent_services = true
 }
 
@@ -29,7 +29,7 @@ resource "google_project_service" "container_api" {
 resource "google_project_service" "compute_engine_api" {
   project = local.project_id
   service = "compute.googleapis.com"
-  
+
   disable_dependent_services = true
 }
 
@@ -37,6 +37,6 @@ resource "google_project_service" "compute_engine_api" {
 resource "google_project_service" "cloud_resource_manager_api" {
   project = local.project_id
   service = "cloudresourcemanager.googleapis.com"
-  
+
   disable_dependent_services = true
 }
