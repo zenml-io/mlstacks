ba774ef84bd70e7ec7f06dc87584ded9d33880a8
Jayesh Sharma <wjayesh@outlook.com>
2022-08-09 17:25:29 +0530

    disable dependent GCP services svcs on destroy

--- gcp-minimal/enable_services.tf
+++ gcp-minimal/enable_services.tf
@@ -5,28 +5,38 @@
 resource "google_project_service" "secret_manager" {
   project = local.project_id
   service = "secretmanager.googleapis.com"
+  
+  disable_dependent_services = true
 }
 
 # enable container registry
 resource "google_project_service" "container_registry" {
   project = local.project_id
   service = "containerregistry.googleapis.com"
+  
+  disable_dependent_services = true
 }
 
 # enable container API
 resource "google_project_service" "container_api" {
   project = local.project_id
   service = "container.googleapis.com"
+  
+  disable_dependent_services = true
 }
 
 # enable compute engine API
 resource "google_project_service" "compute_engine_api" {
   project = local.project_id
   service = "compute.googleapis.com"
+  
+  disable_dependent_services = true
 }
 
 # enable cloud resource manager API
 resource "google_project_service" "cloud_resource_manager_api" {
   project = local.project_id
   service = "cloudresourcemanager.googleapis.com"
+  
+  disable_dependent_services = true
 }
