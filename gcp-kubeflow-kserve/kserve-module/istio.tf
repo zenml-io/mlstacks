e6fee7c554adb678d7c35f29cefcfb37e37ed0c0
Jayesh Sharma <wjayesh@outlook.com>
2022-08-15 22:40:55 +0530

    implement hack to avoid "no envoyfilter CRDs" error

--- gcp-kubeflow-kserve/kserve-module/istio.tf
+++ gcp-kubeflow-kserve/kserve-module/istio.tf
@@ -3,6 +3,11 @@ resource "null_resource" "create-istio-kserve" {
   provisioner "local-exec" {
     command = "kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.6.0/istio.yaml"
   }
+  # repeating the same command as a hack to prevent errors in subsequent commands
+  # running it only once would have caused the next command to fail with no envoyfilter CRDs.
+  provisioner "local-exec" {
+    command = "kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.6.0/istio.yaml"
+  }
   provisioner "local-exec" {
     command = "kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.6.0/istio.yaml"
   }
