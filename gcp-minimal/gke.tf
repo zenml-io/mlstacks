0cb18030acd113ac711545f55bb4140a41b71bd8
Jayesh Sharma <wjayesh@outlook.com>
2022-08-16 23:47:20 +0530

    migrate from gke module to google container cluster

--- gcp-minimal/gke.tf
+++ gcp-minimal/gke.tf
@@ -1,59 +1,57 @@
 data "google_client_config" "default" {}
+resource "google_container_cluster" "gke" {
+  name               = "${local.prefix}-${local.gke.cluster_name}"
+  location           = local.region
+  initial_node_count = 1
 
-module "gke" {
-  source     = "terraform-google-modules/kubernetes-engine/google"
-  project_id = local.project_id
-  name       = "${local.prefix}-${local.gke.cluster_name}"
-  region     = local.region
-  zones      = ["${local.region}-a", "${local.region}-b", "${local.region}-c"]
-  # network                    = "${local.prefix}-${local.vpc.name}"
-  # subnetwork                 = "${local.prefix}-${local.vpc.name}-01"
-  # ip_range_pods              = "${local.region}-01-gke-01-pods"
-  # ip_range_services          = "${local.region}-01-gke-01-services"
-  network           = module.vpc.network_name
-  subnetwork        = module.vpc.subnets_names[0]
-  ip_range_pods     = "gke-pods"
-  ip_range_services = "gke-services"
-
-  http_load_balancing        = false
-  network_policy             = false
-  horizontal_pod_autoscaling = true
-  filestore_csi_driver       = false
-
-  node_pools = [
-    {
-      name            = "default-node-pool"
-      machine_type    = "e2-medium"
-      node_locations  = "${local.region}-b"
-      min_count       = 1
-      max_count       = 3
-      local_ssd_count = 0
-      disk_size_gb    = 100
-      disk_type       = "pd-standard"
-      image_type      = "COS_CONTAINERD"
-      enable_gcfs     = false
-      auto_repair     = true
-      auto_upgrade    = true
-      # service_account           = "${local.prefix}-${local.gke.service_account_name}@${local.project_id}.iam.gserviceaccount.com"
-      preemptible        = false
-      initial_node_count = 1
-    },
-  ]
-
-  node_pools_oauth_scopes = {
-    all = []
-
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
+  node_config {
+    service_account = google_service_account.gke-service-account.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
+    machine_type = "e2-medium"
   }
+}
 
-  node_pools_labels = {
-    all = {}
+# service account for GKE nodes
+resource "google_service_account" "gke-service-account" {
+  account_id   = local.gke.service_account_name
+  project      = local.project_id
+  display_name = "Terraform GKE SA"
+}
 
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
+resource "google_project_iam_binding" "container-registry" {
+  project = local.project_id
+  role    = "roles/containerregistry.ServiceAgent"
 
+  members = [
+    "serviceAccount:${google_service_account.gke-service-account.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "secret-manager" {
+  project = local.project_id
+  role    = "roles/secretmanager.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.gke-service-account.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "cloudsql" {
+  project = local.project_id
+  role    = "roles/cloudsql.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.gke-service-account.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "storageadmin" {
+  project = local.project_id
+  role    = "roles/storage.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.gke-service-account.email}",
+  ]
 }
