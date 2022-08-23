0616767e0d8592f52e468b2d2b80694ecb21db32
Jayesh Sharma <wjayesh@outlook.com>
2022-08-23 14:32:04 +0530

    fix resource name

--- azure-minimal/configure_docker.tf
+++ azure-minimal/configure_docker.tf
@@ -1,5 +1,5 @@
 # set up local docker client to access the newly created registry
-resource "null_resource" "configure-local-kubectl" {
+resource "null_resource" "configure-local-docker" {
   provisioner "local-exec" {
     command = "az acr login --name ${azurerm_container_registry.container_registry.name}"
   }
