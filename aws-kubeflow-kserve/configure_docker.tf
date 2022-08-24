0cb6d969cd822f4d42dc53fd41e8422b5912df40
Jayesh Sharma <wjayesh@outlook.com>
2022-08-24 20:15:24 +0530

    format files

--- aws-kubeflow-kserve/configure_docker.tf
+++ aws-kubeflow-kserve/configure_docker.tf
@@ -3,5 +3,5 @@ resource "null_resource" "configure-local-docker" {
   provisioner "local-exec" {
     command = "aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com"
   }
-  
+
 }
