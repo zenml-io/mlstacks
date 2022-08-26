737b13e2edc0e1cf76761e59d7750cb2a6781da6
Jayesh Sharma <wjayesh@outlook.com>
2022-08-26 19:53:39 +0530

    add information about europe-west1-a

--- gcp-kubeflow-kserve/locals.tf
+++ gcp-kubeflow-kserve/locals.tf
@@ -1,6 +1,11 @@
 # config values to use across the module
 locals {
   prefix     = "demo"
+
+  # if you're using europe-west1, please make the following modification in
+  # the gke.tf file:
+  # For zones in module.gke, replace "${local.region}-a" to "${local.region}-d"
+  # This is because "europe-west1-a" doesn't exist for some reason.
   region     = "europe-west3"
   project_id = "zenml-demos"
 
