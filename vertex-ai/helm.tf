f0fecd8c6a1d2437dd8dc3171bc4f3155e47a08c
Jayesh Sharma <wjayesh@outlook.com>
2022-09-19 14:10:10 +0530

    fix problems when mlflow is disabled

--- vertex-ai/helm.tf
+++ vertex-ai/helm.tf
@@ -1,8 +1,8 @@
 # A default (non-aliased) provider configuration for "helm"
 provider "helm" {
   kubernetes {
-    host                   = "https://${module.gke[0].endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.gke[0].ca_certificate)
+    host                   = local.enable_mlflow? "https://${module.gke[0].endpoint}" : ""
+    token                  = local.enable_mlflow? data.google_client_config.default.access_token : ""
+    cluster_ca_certificate = local.enable_mlflow? base64decode(module.gke[0].ca_certificate) : ""
   }
 }
