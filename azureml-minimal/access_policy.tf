1263b60d465c443800cc69218fbe6116b3501c5d
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2022-09-29 18:56:00 +0530

    assign principal id to key vault policy

--- azureml-minimal/access_policy.tf
+++ azureml-minimal/access_policy.tf
@@ -15,7 +15,7 @@ resource "azurerm_role_assignment" "ra" {
 resource "azurerm_key_vault_access_policy" "kv-access" {
   key_vault_id = azurerm_key_vault.secret_manager.id
   tenant_id    = azurerm_machine_learning_compute_cluster.cluster.identity[0].tenant_id
-  object_id    = data.azurerm_client_config.config.object_id
+  object_id    = azurerm_machine_learning_compute_cluster.cluster.identity[0].principal_id
 
   key_permissions = [
     "Get", "List", "Create", "Delete", "Update"
