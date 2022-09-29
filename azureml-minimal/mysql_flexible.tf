c9fb3b816615855be7156cd18cb343808761b0ca
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2022-09-29 18:33:45 +0530

    make mysql server public

--- azureml-minimal/mysql_flexible.tf
+++ azureml-minimal/mysql_flexible.tf
@@ -5,12 +5,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   administrator_login    = var.metadata-db-username
   administrator_password = var.metadata-db-password == "" ? random_password.mysql_password.result : var.metadata-db-password
   backup_retention_days  = 7
-  delegated_subnet_id    = azurerm_subnet.mysql_vnet.id
-  private_dns_zone_id    = azurerm_private_dns_zone.mysql_dns.id
   sku_name               = "B_Standard_B1s"
-  depends_on = [
-    azurerm_private_dns_zone_virtual_network_link.dns_link
-  ]
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "allow_IPs" {
@@ -24,18 +19,6 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "allow_IPs" {
 resource "random_password" "mysql_password" {
   length  = 12
   special = false
-}
-
-resource "azurerm_private_dns_zone" "mysql_dns" {
-  name                = "zenml.mysql.database.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
-  name                  = "mysqllink"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
-  virtual_network_id    = module.network.vnet_id
 }
 
 # download SSL certificate
