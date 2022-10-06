ce5faeeef57b84c224d07a6928afecfe05dc6b0e
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2022-10-06 13:05:17 +0530

    reuse client configuration

--- azureml-minimal/key_vault.tf
+++ azureml-minimal/key_vault.tf
@@ -1,12 +1,10 @@
-data "azurerm_client_config" "cconfig" {}
-
 # workspace keyvault
 resource "azurerm_key_vault" "secret_manager" {
   name                        = local.key_vault.name
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.cconfig.tenant_id
+  tenant_id                   = data.azurerm_client_config.config.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
