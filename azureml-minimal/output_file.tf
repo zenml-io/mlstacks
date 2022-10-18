40b6905444dee163f47aa75d16017b3260b32c97
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-10-18 13:39:38 +0200

    Fix AzureML step operator config name

--- azureml-minimal/output_file.tf
+++ azureml-minimal/output_file.tf
@@ -21,7 +21,7 @@ resource "local_file" "stack_file" {
         id: ${uuid()}
         flavor: azureml
         name: azureml_step_operator
-        configuration: {"subscription_id": "${data.azurerm_client_config.current.subscription_id}", "resource_group_name": "${azurerm_resource_group.rg.name}", "workspace_name": "${azurerm_machine_learning_workspace.mlw.name}", "compute_target_name": "${azurerm_machine_learning_compute_cluster.cluster.name}"}
+        configuration: {"subscription_id": "${data.azurerm_client_config.current.subscription_id}", "resource_group": "${azurerm_resource_group.rg.name}", "workspace_name": "${azurerm_machine_learning_workspace.mlw.name}", "compute_target_name": "${azurerm_machine_learning_compute_cluster.cluster.name}"}
       secrets_manager:
         id: ${uuid()}
         flavor: azure
