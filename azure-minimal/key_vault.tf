c05a80ed9afb8924dd9960332f4bae974a694eea
Jayesh Sharma <wjayesh@outlook.com>
2022-10-18 19:44:36 +0530

    add access policy for key vault

--- azure-minimal/key_vault.tf
+++ azure-minimal/key_vault.tf
@@ -29,3 +29,40 @@ resource "azurerm_key_vault" "secret_manager" {
     ]
   }
 }
+
+
+resource "azurerm_key_vault_access_policy" "kv-access" {
+  key_vault_id = azurerm_key_vault.secret_manager.id
+  tenant_id    = azurerm_kubernetes_cluster.aks.identity[0].tenant_id
+  object_id    = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+
+  key_permissions = [
+    "Get", "List", "Create", "Delete", "Update"
+  ]
+
+  secret_permissions = [
+    "Get", "List", "Set", "Delete"
+  ]
+
+  storage_permissions = [
+    "Get", "List", "Set", "Delete", "Update"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "kv-access-user" {
+  key_vault_id = azurerm_key_vault.secret_manager.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get", "List", "Create", "Delete", "Update"
+  ]
+
+  secret_permissions = [
+    "Get", "List", "Set", "Delete"
+  ]
+
+  storage_permissions = [
+    "Get", "List", "Set", "Delete", "Update"
+  ]
+}
