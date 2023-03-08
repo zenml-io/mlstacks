c49b532f066e89150211cb67ec965ca4bbcbf6db
Jayesh Sharma <wjayesh@outlook.com>
2023-03-08 14:55:41 +0530

    check if data source is null before accessing

--- gcp-modular/kubernetes.tf
+++ gcp-modular/kubernetes.tf
@@ -1,15 +1,15 @@
 # a default (non-aliased) provider configuration for "kubernetes"
 # not defining the kubernetes provider throws an error while running the eks module
 provider "kubernetes" {
-  host                   = length(data.external.get_cluster.result) > 0 ? data.external.get_cluster.result["endpoint"] : ""
+  host                   = (data.external.get_cluster.result != null) ? data.external.get_cluster.result["endpoint"] : ""
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = length(data.external.get_cluster.result) > 0 ? base64decode(data.external.get_cluster.result["ca_certificate"]) : ""
+  cluster_ca_certificate = (data.external.get_cluster.result != null) ? base64decode(data.external.get_cluster.result["ca_certificate"]) : ""
 }
 
 provider "kubectl" {
-  host                   = length(data.external.get_cluster.result) > 0 ? data.external.get_cluster.result["endpoint"] : ""
+  host                   = (data.external.get_cluster.result != null) ? data.external.get_cluster.result["endpoint"] : ""
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = length(data.external.get_cluster.result) > 0 ? base64decode(data.external.get_cluster.result["ca_certificate"]) : ""
+  cluster_ca_certificate = (data.external.get_cluster.result != null) ? base64decode(data.external.get_cluster.result["ca_certificate"]) : ""
 }
 
 # the namespace where zenml will run kubernetes orchestrator workloads
