3a8c913d5764496c7398f516b58610ab890d2fbe
Jayesh Sharma <wjayesh@outlook.com>
2023-03-08 15:13:52 +0530

    check if data source is null before accessing (#39)

--- aws-modular/helm.tf
+++ aws-modular/helm.tf
@@ -1,8 +1,8 @@
 # A default (non-aliased) provider configuration for "helm"
 provider "helm" {
   kubernetes {
-    host                   = length(data.external.get_cluster_info.result) > 0 ? data.external.get_cluster_info.result["endpoint"] : ""
-    cluster_ca_certificate = length(data.external.get_cluster_info.result) > 0 ? base64decode(data.external.get_cluster_info.result["ca_certificate"]) : ""
-    token                  = length(data.external.get_cluster_auth.result) > 0 ? data.external.get_cluster_auth.result["token"] : ""
+    host                   = (data.external.get_cluster_info.result != null) ? data.external.get_cluster_info.result["endpoint"] : ""
+    cluster_ca_certificate = (data.external.get_cluster_info.result != null) ? base64decode(data.external.get_cluster_info.result["ca_certificate"]) : ""
+    token                  = (data.external.get_cluster_auth.result != null) ? data.external.get_cluster_auth.result["token"] : ""
   }
 }
