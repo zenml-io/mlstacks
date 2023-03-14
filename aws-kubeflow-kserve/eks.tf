eac9ad186bf8f1828b2309d097d3a659bd228e61
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-03-14 08:40:01 +0100

    fix typos

--- aws-kubeflow-kserve/eks.tf
+++ aws-kubeflow-kserve/eks.tf
@@ -1,4 +1,4 @@
-# eks module to creater a cluster
+# eks module to create a cluster
 # newer versions of it had some error so going with v17.23.0 for now
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
