3cb1f043a74b59417a227cbc3f73fe91a11c24d5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-21 18:57:25 +0200

    update parsing

--- src/stackrecipes/utils/terraform_utils.py
+++ src/stackrecipes/utils/terraform_utils.py
@@ -1,5 +1,6 @@
-from typing import Dict
+from typing import Dict, List
 import python_terraform
+from stackrecipes.models.component import Component
 from stackrecipes.models.stack import Stack
 
 
@@ -11,9 +12,18 @@ class TerraformRunner:
             working_dir=self.tf_recipe_path,
         )
 
+
 def parse_component_variables(components: List[Component]) -> Dict[str, str]:
     component_variables = {}
     for component in components:
+        if (
+            component.component_type == "artifact_store"
+            and component.metadata.config
+        ):
+            component_variables["bucket_name"] = component.metadata.config.get(
+                "bucket_name"
+            )
+            component_variables["enable_artifact_store"] = "true"
         # extract the logic for each different component type
         # add those variables to the component_variables dict
 
@@ -23,6 +33,7 @@ def parse_component_variables(components: List[Component]) -> Dict[str, str]:
 def parse_tf_vars(stack: Stack) -> Dict[str, str]:
     tf_vars = {
         "region": stack.default_region,
+        "tags": stack.default_tags,
     }
     # update the dict with the component variables
     tf_vars.update(parse_component_variables(stack.components))
