ec5c1aa3892793927bc7eafe48143597e2823737
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-26 11:26:43 +0200

    reduce linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,7 @@ SRC=${1:-"src/mlstacks tests"}
 SRC_NO_TESTS=${1:-"src/mlstacks"}
 TESTS=${1:-"tests"}
 
-ruff $SRC
+ruff $SRC_NO_TESTS
 
 # autoflake replacement: checks for unused imports and variables
 ruff $SRC --select F401,F841 --exclude "__init__.py" --isolated
