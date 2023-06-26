4b054f74164528162b04849cafd3887578e1a298
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-26 15:58:29 +0200

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+# Test Commang
 poetry publish --build --repository testpypi --dry-run --username $PYPI_USERNAME --password $PYPI_PASSWORD
+
+# Real Command
+# poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
