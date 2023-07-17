4bbdadafec54e9c85b5e8135c5033a2954426c2c
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-07-17 13:54:20 +0200

    update stack tests

--- tests/models/test_stack.py
+++ tests/models/test_stack.py
@@ -1,5 +1,6 @@
 from hypothesis import given
 from hypothesis import strategies as st
+from mlstacks.enums import DeploymentMethodEnum, ProviderEnum
 
 from mlstacks.models.stack import Stack
 
@@ -9,6 +10,15 @@ def test_component(instance):
     assert isinstance(instance.spec_version, int)
     assert isinstance(instance.spec_type, str)
     assert isinstance(instance.name, str)
-    assert isinstance(instance.provider, str)
+    assert isinstance(instance.provider, ProviderEnum)
     assert instance.components is not None
     assert isinstance(instance.components, list)
+    assert (
+        isinstance(instance.default_region, str)
+        or instance.default_region is None
+    )
+    assert (
+        isinstance(instance.default_tags, dict)
+        or instance.default_tags is None
+    )
+    assert isinstance(instance.deployment_method, DeploymentMethodEnum)
