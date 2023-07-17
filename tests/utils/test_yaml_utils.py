8a6ce22407c71f8cc5d7d9191d6f5bb3af788ad5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-07-17 15:12:24 +0200

    add yaml utils tests

--- tests/utils/test_yaml_utils.py
+++ tests/utils/test_yaml_utils.py
@@ -1,1 +1,16 @@
-"""Tests for YAML Utils."""
+import os
+import tempfile
+
+from mlstacks.utils.yaml_utils import load_yaml_as_dict
+
+
+def test_load_valid_yaml_file():
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
+        f.write("name: John\nage: 30")
+        f_path = f.name
+    try:
+        expected = {"name": "John", "age": 30}
+        result = load_yaml_as_dict(f_path)
+        assert result == expected
+    finally:
+        os.remove(f_path)
