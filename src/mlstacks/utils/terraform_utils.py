7ea4251d1e77b9c85dcb552ddeb02dcb96d8ccf5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-07 14:05:27 +0200

    fix debug mode for destroy

--- src/mlstacks/utils/terraform_utils.py
+++ src/mlstacks/utils/terraform_utils.py
@@ -398,20 +398,23 @@ def tf_client_destroy(
     if debug:
         ret_code, _stdout, _stderr = client.destroy(
             var=tf_vars,
+            input=True,
             capture_output=False,
             raise_on_error=True,
             force=python_terraform.IsNotFlagged,
             refresh=False,
             auto_approve=False,
+            skip_plan=False,
         )
     else:
         ret_code, _stdout, _stderr = client.destroy(
             var=tf_vars,
             capture_output=True,
-            raise_on_error=True,
+            raise_on_error=False,
             force=python_terraform.IsNotFlagged,
             refresh=False,
             auto_approve=True,
+            skip_plan=True,
         )
     return ret_code, _stdout, _stderr
 
