329bb10615e44daf6de3a0aea11661911f297439
Safoine El Khabich <safoine.ext@zenml.io>
2023-08-18 13:19:55 +0100

    add terraform backend to store the state

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -8,6 +8,13 @@ terraform {
   }
 
   required_version = ">= 0.14.8"
+
+  backend "azurerm" {
+    resource_group_name  = "zenml-developers"
+    storage_account_name = "zenmlstorageaccount"
+    container_name       = "github-runner-tf"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
