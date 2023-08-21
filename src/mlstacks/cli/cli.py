5001695fe79a1ee43802b51f9b750c891346f62a
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-21 15:56:03 +0200

    add catch for missing TF stack files

--- src/mlstacks/cli/cli.py
+++ src/mlstacks/cli/cli.py
@@ -25,6 +25,7 @@ from mlstacks.utils.cli_utils import (
     _get_spec_dir,
     confirmation,
     declare,
+    error,
     pretty_print_output_vals,
 )
 from mlstacks.utils.terraform_utils import (
@@ -112,7 +113,10 @@ def destroy(file: str, debug: bool = False, yes: bool = False) -> None:
     stack_name: str = str(yaml_dict.get("name"))
     provider: str = str(yaml_dict.get("provider"))
     declare(f"Destroying stack '{stack_name}' from '{file}'...")
-    destroy_stack(stack_path=file, debug_mode=debug)
+    try:
+        destroy_stack(stack_path=file, debug_mode=debug)
+    except ValueError:
+        error("Couldn't find stack files to destroy.")
 
     spec_files_dir: str = _get_spec_dir(stack_name)
     tf_files_dir: str = _get_tf_recipe_path(provider)
