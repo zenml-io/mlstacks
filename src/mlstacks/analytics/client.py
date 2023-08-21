36e848e0994a361dabf5bb3438e84e395c14f5fa
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-21 10:47:41 +0200

    add tracking helper utils and methods

--- src/mlstacks/analytics/client.py
+++ src/mlstacks/analytics/client.py
@@ -13,25 +13,58 @@
 """Analytics module for MLStacks."""
 
 import datetime
+import os
+from typing import Optional
+from uuid import uuid4
 
+import click
 import segment.analytics as analytics
+
+from mlstacks.constants import MLSTACKS_PACKAGE_NAME
+from mlstacks.utils.yaml_utils import load_yaml_as_dict
 
 analytics.write_key = "tU9BJvF05TgC29xgiXuKF7CuYP0zhgnx"
 
-user_id = "f4ca124298"
-analytics.identify(
-    user_id,
-    {
-        "name": "Alex Strick van Linschoten",
-        "email": "alex.ext@zenml.io",
-        "created_at": datetime.datetime.now(),
-    },
-)
+CONFIG_FILENAME = "config.yaml"
 
-# analytics.track(
-#     user_id,
-#     AnalyticsEventsEnum.MLSTACKS_SOURCE,
-#     {
-#         "python_version": python_version(),
-#     },
-# )
+
+def get_analytics_user_id() -> Optional[str]:
+    """Returns the user id for analytics.
+
+    Returns:
+        str: user id
+    """
+    # read user_id from config_file
+    config_dir = click.get_app_dir(MLSTACKS_PACKAGE_NAME)
+    config_file = os.path.join(config_dir, CONFIG_FILENAME)
+    if os.path.exists(config_file):
+        yaml_dict = load_yaml_as_dict(config_file)
+        return yaml_dict.get("analytics_user_id", None)
+
+
+def set_analytics_user_id(user_id: str) -> None:
+    """Sets the user id for analytics.
+
+    Args:
+        user_id (uuid4): user id
+    """
+    config_dir = click.get_app_dir(MLSTACKS_PACKAGE_NAME)
+    os.makedirs(config_dir, exist_ok=True)
+    config_file = os.path.join(config_dir, CONFIG_FILENAME)
+    # write user_id to config_file
+    with open(config_file, "w") as f:
+        f.write(f"analytics_user_id: {user_id}")
+
+
+if (
+    not os.environ.get("MLSTACKS_ANALYTICS_OPT_OUT")
+    and not get_analytics_user_id()
+):
+    user_id = str(uuid4())
+    set_analytics_user_id(user_id)
+    analytics.identify(
+        user_id,
+        {
+            "created_at": datetime.datetime.now(),
+        },
+    )
