6ddda37b1f7cffe17cfa658b01102886cc2077c8
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-22 11:25:50 +0200

    add test for error function

--- tests/utils/test_cli_utils.py
+++ tests/utils/test_cli_utils.py
@@ -14,11 +14,12 @@
 import os
 
 import click
+import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
 from mlstacks.constants import MLSTACKS_PACKAGE_NAME, PERMITTED_NAME_REGEX
-from mlstacks.utils.cli_utils import _get_spec_dir
+from mlstacks.utils.cli_utils import _get_spec_dir, error
 
 SPEC_BASE_DIR = click.get_app_dir(MLSTACKS_PACKAGE_NAME)
 
@@ -28,3 +29,8 @@ def test_get_spec_dir(stack_name: str):
     spec_dir = _get_spec_dir(stack_name)
     assert spec_dir == os.path.join(SPEC_BASE_DIR, f"stack_specs/{stack_name}")
     assert isinstance(spec_dir, str)
+
+
+def test_error_raises_error():
+    with pytest.raises(click.ClickException):
+        error("error message")
