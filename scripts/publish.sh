db601e990908d0e5dae12cce882d6f40f95583ae
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-23 11:15:19 +0200

    set poetry publication to live

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Test Command
-poetry publish --build --repository test-pypi
-# add `--dry-run` if you don't want it to do anything
+# # Test Command
+# poetry publish --build --repository test-pypi
+# # add `--dry-run` if you don't want it to do anything
 
-# Real Command
-# poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
+# # Real Command
+poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
