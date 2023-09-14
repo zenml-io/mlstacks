0341107071fa8687a1a420bee5f1d14f4e520663
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-09-14 17:13:09 +0200

    update gcp definition

--- src/mlstacks/terraform/gcp-remote-state/main.tf
+++ src/mlstacks/terraform/gcp-remote-state/main.tf
@@ -4,16 +4,15 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "terraform_state" {
-  name          = var.bucket_name
-  location      = var.region
-  storage_class = "STANDARD"
+  name     = var.bucket_name
+  location = var.region
 
   versioning {
     enabled = true
   }
 
   # set to true when you want to delete the bucket
-  force_destroy = false
+  # force_destroy = true
 
   # Ensure no public access
   uniform_bucket_level_access = true
