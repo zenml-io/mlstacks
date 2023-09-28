2fec03d7bf5416d549c4d4701f010965e6230e9b
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-09-28 15:31:51 +0200

    make aws remote state use module

--- src/mlstacks/terraform/aws-remote-state/variables.tf
+++ src/mlstacks/terraform/aws-remote-state/variables.tf
@@ -7,3 +7,15 @@ variable "bucket_name" {
   description = "The name of the S3 bucket to deploy"
   default     = ""
 }
+
+variable "force_destroy" {
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
+  default     = false
+  type        = bool
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the bucket"
+  type        = map(string)
+  default     = {}
+}
