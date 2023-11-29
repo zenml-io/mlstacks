ad4a999d5e19ac2526b987b1555503ea4ee70351
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-11-29 11:21:58 +0100

    bump default zenml versions

--- src/mlstacks/terraform/aws-modular/variables.tf
+++ src/mlstacks/terraform/aws-modular/variables.tf
@@ -94,7 +94,7 @@ variable "mlflow_bucket" {
 # variables for creating a ZenML stack configuration file
 variable "zenml-version" {
   description = "The version of ZenML being used"
-  default     = "0.12.0"
+  default     = "0.50.0"
   type        = string
 }
 
