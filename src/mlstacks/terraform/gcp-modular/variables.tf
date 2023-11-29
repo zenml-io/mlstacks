1ca9548f7a5c1e8dd0d62e55a1433a2642cfe087
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-11-29 13:18:24 +0100

    bump default zenml versions (#117)

--- src/mlstacks/terraform/gcp-modular/variables.tf
+++ src/mlstacks/terraform/gcp-modular/variables.tf
@@ -86,7 +86,7 @@ variable "mlflow-password" {
 # variables for creating a ZenML stack configuration file
 variable "zenml-version" {
   description = "The version of ZenML being used"
-  default     = "0.12.0"
+  default     = "0.50.0"
   type        = string
 }
 
