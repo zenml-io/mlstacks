7b016c85f913fefe979d969bcaab1fd21cec76e7
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-02-19 16:11:23 +0100

    add deprecation notice

--- aws-kubeflow-kserve/aws_account.tf
+++ aws-kubeflow-kserve/aws_account.tf
@@ -1,2 +1,6 @@
+# DEPRECATION WARNING: This code has been deprecated
+# The maintained & current code can be found at src/mlstacks/terraform/
+# under the same relative location.
+
 # fetch the current account ID for use in container registry URL
 data "aws_caller_identity" "current" {}
