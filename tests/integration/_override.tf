4cc25023d61241c6e1cee16692e41bd111041677
Marwan Zaarab <99911676+marwan37@users.noreply.github.com>
2024-02-19 08:16:08 -0600

    Integrate with `localstack` for testing AWS deployments (#145)
    * Setup integration testing directories for AWS deployments
    
    * Add local.tfvars for modular and remote-state integration testing
    
    * Add verify_stack.sh scripts for AWS modular and remote state integration tests
    
    * Add __init__.py in tests/integration
    
    * Create GitHub Actions workflow for AWS integration testing
    
    * Add docker-compose.localstack.yml for LocalStack service emulation
    
    * Run format and lint scripts
    
    * Add README.md for integration tests documentation
    
    * Address typo in README
    
    * Add removal of localstack_providers_override.tf in GitHub Actions cleanup step
    
    * Revise README for enhanced Docker setup instructions and overall documentation clarity
    
    * Modified CI workflow to fix path issues, emulate tflocal, and ensure proper cleanup
    
    * Add _override.tf for LocalStack integration in tests
    
    * Improve verify_stack.sh scripts with debugging outputs
    
    * Integrate localstack-aws-integratin-test job into main CI workflow
    
    * Make aws-remote-state/verify_stack.sh executable
    
    * Add workflow dispatch trigger to workflow file
    
    * Fix path in cleanup step to correctly delete _override.tf for aws-remote-state
    
    * Update README to reflect workflow and testing approach modifications
    
    * Remove debug line displaying absolute YAML path from workflow and README
    
    * Update .github/workflows/aws-integration-test.yml
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Update tests/integration/aws-modular/local.tfvars
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Update tests/integration/aws-remote-state/local.tfvars
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Update tests/integration/README.md
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Remove cleanup steps from AWS Modular workflow
    
    Following @strickvl's suggestion, removed the terraform destroy and related cleanup commands. These steps are unnecessary for our ephemeral instances and do not impact the repo's state.
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Update README to reflect changes to terraform version and removal of cleanup steps
    
    ---------
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>

---
+++ tests/integration/_override.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "eu-north-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    apigatewayv2   = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
